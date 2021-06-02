85b4717e86559bf7d28094524b0a52758205291d
Peter Uhrecky <peter.uhrecky@nike.sk>
2021-06-02 20:03:16 +0200

    collections list vector map

--- scala/test/src/main/scala/collections.scala
+++ scala/test/src/main/scala/collections.scala
@@ -1,4 +1,5 @@
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.Map
 
 object collections {
     def main(args: Array[String]): Unit = {
@@ -8,6 +9,33 @@ object collections {
         intArray(2) = 4
         println(intArray)
 
+        val charList = List('a', 'b', 'c') // linked list
+        println(charList(1)) // slow, not indexed sequence (use vector)
+        val charList2 = 'y' +: charList // can be charList :+ 'y'
+        val charList3 = List('x', 'z') ++: charList2
+        println(charList3)
+        val strList = "lol" :: "omg" :: List()
+        println(strList)
+        val strList2 = "xD" :: Nil
+        println(strList2 :: strList)
+        println(5 :: strList2 :: strList :: Nil)
 
+        println(Vector('a', 1) :++ List("xz"))
+
+        val myMap = collection.mutable.Map( // type not necessary
+            'x' -> 15,
+            1 -> 27,
+            "o" -> 'b',
+            5 -> new MyFullClass(5)
+        )
+        myMap += Tuple2('c', 'd')
+        myMap ++= Map('e' -> 'f')
+        myMap -= (1, 'x') // remove theese keys
+        println(myMap)
+
+        myMap.foreach {
+            case(key : Char, value : Char) => println(s"found pair char - char : $key, $value")
+            case _ => println("other pair")
+        }
     }
 }
