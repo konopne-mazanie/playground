70edb7ff36a8c538c0ad37dc3c2ac6ab7d49404c
Peter Uhrecky <peter.uhrecky@nike.sk>
2021-06-13 02:18:55 +0200

    akka unix socket

--- scala/ocr_connect/build.sbt
+++ scala/ocr_connect/build.sbt
@@ -6,5 +6,7 @@ scalaVersion := "2.13.6"
 
 libraryDependencies += "com.typesafe.akka" %% "akka-actor-typed" % "2.6.15"
 libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.3"
+libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.6.15"
+libraryDependencies += "com.lightbend.akka" %% "akka-stream-alpakka-unix-domain-socket" % "3.0.1"
 // "com.typesafe.akka" %% "akka-actor-testkit-typed" % akkaVersion % Test,
 // "org.scalatest" %% "scalatest" % "3.1.0" % Test
--- scala/ocr_connect/src/main/scala/OcrConnect/OcrConnect.scala
+++ scala/ocr_connect/src/main/scala/OcrConnect/OcrConnect.scala
@@ -1,7 +1,36 @@
 package OcrConnect
 
+import java.io.File
+
+import scala.concurrent.{Future, Promise}
+
+import akka.stream.OverflowStrategy
+import akka.stream.alpakka.unixdomainsocket.scaladsl.UnixDomainSocket
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.util.ByteString
+
 object OcrConnect {
     def main(args: Array[String]): Unit = {
-        val socket = new Socket()
+        //val socket = new Socket()
+        val path: java.nio.file.Path = new File("/tmp/test.sock").toPath
+        val received = Promise[ByteString]
+
+        val serverSideFlow = Flow[ByteString]
+            .buffer(1, OverflowStrategy.backpressure)
+            .wireTap(bytes => received.success(bytes))
+
+        //#binding
+        val binding: Future[UnixDomainSocket.ServerBinding] =
+            UnixDomainSocket().bindAndHandle(serverSideFlow, path)
+        //#binding
+
+        //#outgoingConnection
+        val sendBytes = ByteString("Hello")
+        binding.flatMap { _ => // connection
+            Source
+                .single(sendBytes)
+                .via(UnixDomainSocket().outgoingConnection(path))
+                .runWith(Sink.ignore)
+        }
     }
 }
