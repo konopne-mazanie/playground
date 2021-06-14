11adab7b62b51ae5569e1e94b12e9e165813b66d
Peter Uhrecky <peter.uhrecky@nike.sk>
2021-06-14 02:12:26 +0200

    akka unix socket

--- scala/ocr_connect/build.sbt
+++ scala/ocr_connect/build.sbt
@@ -8,5 +8,5 @@ libraryDependencies += "com.typesafe.akka" %% "akka-actor-typed" % "2.6.15"
 libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.3"
 libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.6.15"
 libraryDependencies += "com.lightbend.akka" %% "akka-stream-alpakka-unix-domain-socket" % "3.0.1"
-// "com.typesafe.akka" %% "akka-actor-testkit-typed" % akkaVersion % Test,
+libraryDependencies += "com.typesafe.akka" %% "akka-actor-testkit-typed" % "2.6.15"
 // "org.scalatest" %% "scalatest" % "3.1.0" % Test
--- scala/ocr_connect/src/main/scala/OcrConnect/OcrConnect.scala
+++ scala/ocr_connect/src/main/scala/OcrConnect/OcrConnect.scala
@@ -1,36 +1,17 @@
 package OcrConnect
 
 import java.io.File
-
-import scala.concurrent.{Future, Promise}
-
-import akka.stream.OverflowStrategy
 import akka.stream.alpakka.unixdomainsocket.scaladsl.UnixDomainSocket
-import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
+import akka.testkit.TestKit
 import akka.util.ByteString
 
-object OcrConnect {
+object OcrConnect extends TestKit(ActorSystem("UnixDomainSocketSpec")) {
     def main(args: Array[String]): Unit = {
-        //val socket = new Socket()
-        val path: java.nio.file.Path = new File("/tmp/test.sock").toPath
-        val received = Promise[ByteString]
-
-        val serverSideFlow = Flow[ByteString]
-            .buffer(1, OverflowStrategy.backpressure)
-            .wireTap(bytes => received.success(bytes))
-
-        //#binding
-        val binding: Future[UnixDomainSocket.ServerBinding] =
-            UnixDomainSocket().bindAndHandle(serverSideFlow, path)
-        //#binding
-
-        //#outgoingConnection
+        println("start")
+        val path: java.nio.file.Path = new File("/tmp/test.socket").toPath
         val sendBytes = ByteString("Hello")
-        binding.flatMap { _ => // connection
-            Source
-                .single(sendBytes)
-                .via(UnixDomainSocket().outgoingConnection(path))
-                .runWith(Sink.ignore)
-        }
+        Source.single(sendBytes).via(UnixDomainSocket().outgoingConnection(path))
     }
 }
