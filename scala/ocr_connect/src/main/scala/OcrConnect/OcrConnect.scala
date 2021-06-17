4fc4124bd60c1087fd4d5d0733293ffcf13f3195
Peter Uhrecky <peter.uhrecky@nike.sk>
2021-06-17 16:16:31 +0200

    socket client send hello working

--- scala/ocr_connect/src/main/scala/OcrConnect/OcrConnect.scala
+++ scala/ocr_connect/src/main/scala/OcrConnect/OcrConnect.scala
@@ -1,17 +1,36 @@
 package OcrConnect
 
-import java.io.File
-import akka.stream.alpakka.unixdomainsocket.scaladsl.UnixDomainSocket
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
+import akka.stream.Materializer
+import akka.stream.alpakka.unixdomainsocket.scaladsl.UnixDomainSocket
+import akka.stream.scaladsl.Source
 import akka.testkit.TestKit
 import akka.util.ByteString
+
+import java.io.File
+import scala.concurrent.ExecutionContext
+import scala.util.{Failure, Success}
 
 object OcrConnect extends TestKit(ActorSystem("UnixDomainSocketSpec")) {
     def main(args: Array[String]): Unit = {
         println("start")
         val path: java.nio.file.Path = new File("/tmp/test.socket").toPath
-        val sendBytes = ByteString("Hello")
-        Source.single(sendBytes).via(UnixDomainSocket().outgoingConnection(path))
+
+        implicit val ma: Materializer = Materializer(system)
+        implicit val ec: ExecutionContext = system.dispatcher
+
+        val socketFlow = UnixDomainSocket().outgoingConnection(path)
+        val out = Source.single(ByteString("Hello")).via(socketFlow).run()
+        out.onComplete {
+            case Success(_) => println("sent")
+            case Failure(exception) => println(s"failed: $exception")
+        }
+
+        Thread.sleep(5000)
+
+        println("end")
     }
+    // https://github.com/djm1329/AkkaStreamSocketTests/blob/master/src/main/scala/SocketClient.scala
+    // https://doc.akka.io/docs/alpakka/current/unix-domain-socket.html
+    // https://github.com/akka/alpakka/blob/master/unix-domain-socket/src/test/scala/docs/scaladsl/UnixDomainSocketSpec.scala
 }
