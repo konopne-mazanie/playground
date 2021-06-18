44e39499ef15a89ad5e323afed1ef8ca6d8172a9
Peter Uhrecky <peter.uhrecky@nike.sk>
2021-06-18 18:29:40 +0200

    socket client receive working

--- scala/ocr_connect/src/main/scala/OcrConnect/OcrConnect.scala
+++ scala/ocr_connect/src/main/scala/OcrConnect/OcrConnect.scala
@@ -20,17 +20,11 @@ object OcrConnect extends TestKit(ActorSystem("UnixDomainSocketSpec")) {
         implicit val ec: ExecutionContext = system.dispatcher
 
         val socketFlow = UnixDomainSocket().outgoingConnection(path)
-        val out = Source.single(ByteString("Hello")).via(socketFlow).run()
+        val out = Source.single(ByteString("Hello")).via(socketFlow).map(s => println(s.utf8String)).run
         out.onComplete {
             case Success(_) => println("sent")
             case Failure(exception) => println(s"failed: $exception")
         }
 
-        Thread.sleep(5000)
-
-        println("end")
     }
-    // https://github.com/djm1329/AkkaStreamSocketTests/blob/master/src/main/scala/SocketClient.scala
-    // https://doc.akka.io/docs/alpakka/current/unix-domain-socket.html
-    // https://github.com/akka/alpakka/blob/master/unix-domain-socket/src/test/scala/docs/scaladsl/UnixDomainSocketSpec.scala
 }
